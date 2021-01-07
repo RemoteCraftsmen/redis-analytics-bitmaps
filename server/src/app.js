8fd552263693d627df2f7b4331b6eb7198ee3be9
adrian-larysz-rmtcfm-com <adrian.larysz@remotecraftsmen.com>
2021-01-07 15:40:24 +0100

    Server init

---
+++ server/src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+
+module.exports = app;
