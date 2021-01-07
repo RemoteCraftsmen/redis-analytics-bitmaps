e6d0f4f5cba9dfedfc7e7e0b7a05ed9a3b7d143b
adrian-larysz-rmtcfm-com <adrian.larysz@remotecraftsmen.com>
2021-01-07 13:58:20 +0100

    Added components

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,22 @@
 <template>
-    <div class="home"> Abc </div>
+    <v-container fluid>
+        <v-row>
+            <v-col cols="3">
+                <the-analytics-demo-form />
+            </v-col>
+
+            <v-col cols="9">
+                <the-analytics-charts />
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+    components: {
+        theAnalyticsDemoForm: () => import('@/components/TheAnalyticsDemoForm'),
+        theAnalyticsCharts: () => import('@/components/TheAnalyticsCharts')
+    }
+};
 </script>
