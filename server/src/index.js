9ed4004aa018d843116e7e86e99f00dcf5b5ea89
Rafal Ksiazek <rafal.ksiazek@remotecraftsmen.com>
2021-01-20 12:54:04 +0100

    Preparing deploy

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,35 @@
-const app = require('./app');
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+require('express-async-errors');
+const di = require('./di');
 const config = require('./config');
+const errorHandler = require('./plugins/errorHandler');
+
+const app = express();
+
+app.use(helmet());
+app.use(
+    cors({
+        origin(origin, callback) {
+            callback(null, true);
+        },
+        credentials: true
+    })
+);
+app.use(bodyParser.json());
+
+require('./plugins/dayjs')();
+
+const router = require('./routes')(di);
+
+app.use('/', express.static(path.join(__dirname, '../../client-dist')));
+
+app.use('/api', router);
+
+app.use(errorHandler);
 
 const { port } = config.server;
 
