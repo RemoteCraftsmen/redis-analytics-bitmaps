547117f3f5754086a9a3027a0bb3661bbcc8bd0d
adrian-larysz-rmtcfm-com <adrian.larysz@remotecraftsmen.com>
2021-02-12 18:13:26 +0100

    Added script for generating fake, sample data

---
+++ server/src/bin/sample.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+const faker = require('faker');
+const dayjs = require('dayjs');
+
+const samplePath = path.join(__dirname, '..', 'sample.json');
+
+const actions = [
+    { action: 'register' },
+    { action: 'visit', page: 'homepage' },
+    { action: 'visit', page: 'product1' },
+    { action: 'visit', page: 'product2' },
+    { action: 'visit', page: 'product3' },
+    { action: 'addToCart', page: 'product1' },
+    { action: 'addToCart', page: 'product2' },
+    { action: 'addToCart', page: 'product3' },
+    { action: 'buy', page: 'product1' },
+    { action: 'buy', page: 'product2' },
+    { action: 'buy', page: 'product3' }
+];
+
+const sources = ['google', 'facebook', 'email', 'direct', 'referral', 'none'];
+
+const usersCount = 20;
+
+const period = { from: '2015-12-01', to: '2015-12-31' };
+
+const actionsCount = 800;
+
+const data = [];
+
+for (let i = 0; i < actionsCount; i++) {
+    data.push({
+        actionParams: faker.random.arrayElement(actions),
+        date: dayjs(faker.date.between(period.from, period.to)).format('YYYY-MM-DD'),
+        source: faker.random.arrayElement(sources),
+        userId: faker.random.number(usersCount)
+    });
+}
+
+fs.writeFileSync(samplePath, JSON.stringify(data, null, 4));
