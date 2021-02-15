16a75ec07b3e68b6edc40c0b1800c1e311742f03
adrian-larysz-rmtcfm-com <adrian.larysz@remotecraftsmen.com>
2021-02-15 17:46:45 +0100

    Modified sample.json and script for generate

--- server/src/scripts/sample.js
+++ server/src/scripts/sample.js
@@ -8,30 +8,49 @@ const periodService = di.get('services.period');
 
 const samplePath = path.join(__dirname, '..', 'sample.json');
 
-const actions = [
-    { action: 'register' },
-    { action: 'visit', page: 'homepage' },
-    { action: 'visit', page: 'product1' },
-    { action: 'visit', page: 'product2' },
-    { action: 'visit', page: 'product3' },
-    { action: 'addToCart', page: 'product1' },
-    { action: 'addToCart', page: 'product2' },
-    { action: 'addToCart', page: 'product3' },
-    { action: 'addToCart', page: 'product1' },
-    { action: 'addToCart', page: 'product2' },
-    { action: 'addToCart', page: 'product3' },
-    { action: 'buy', page: 'product1' },
-    { action: 'buy', page: 'product2' },
-    { action: 'buy', page: 'product3' }
-];
+const actionHelpers = {
+    visit: (data, date, source, userId, page) => {
+        data.push({ date, source, userId, actionParams: { action: 'visit', page } });
+    },
+
+    register: (data, date, source, userId) => {
+        data.push({ date, source, userId, actionParams: { action: 'register' } });
+    },
+
+    addToCart: (data, date, source, userId, page) => {
+        data.push({ date, source, userId, actionParams: { action: 'addToCart', page } });
+    },
+
+    registerAndBuy: (data, date, source, userId, page) => {
+        actionHelpers.register(data, date, source, userId);
+
+        actionHelpers.addToCart(data, date, source, userId, page);
+
+        data.push({ date, source, userId, actionParams: { action: 'buy', page } });
+    },
+
+    getPage: action => {
+        if (action === 'register') {
+            return;
+        }
+
+        if (action === 'visit') {
+            return faker.random.arrayElement(['product1', 'product2', 'product3', 'homepage']);
+        }
+
+        return faker.random.arrayElement(['product1', 'product2', 'product3']);
+    }
+};
+
+const actions = ['visit', 'register', 'addToCart', 'registerAndBuy'];
 
 const dates = periodService.getRangeOfDates('2015-12-01', '2015-12-31', 'day').map(date => date.format('YYYY-MM-DD'));
 
 const sources = ['google', 'facebook', 'email', 'direct', 'referral', 'none'];
 
-const users = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const users = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
 
-const maxUsersPerDay = 2;
+const maxUsersPerDay = 8;
 
 const data = [];
 
@@ -39,27 +58,12 @@ for (const date of dates) {
     const _users = faker.random.arrayElements(users, faker.random.number(maxUsersPerDay - 1) + 1);
 
     for (const userId of _users) {
-        const actionParams = faker.random.arrayElement(actions);
+        const action = faker.random.arrayElement(actions);
         const source = faker.random.arrayElement(sources);
+        const page = actionHelpers.getPage(action);
 
-        data.push({ date, source, actionParams, userId });
+        actionHelpers[action](data, date, source, userId, page);
     }
 }
-
-data.sort((a, b) => {
-    if (a.date === b.date) {
-        if (a.actionParams.action === 'register' && b.actionParams !== 'register') {
-            return -1;
-        }
-
-        if (a.actionParams.action !== 'register' && b.actionParams === 'register') {
-            return 1;
-        }
-
-        return 0;
-    }
-
-    return dayjs(a.date).isAfter(b.date) ? 1 : -1;
-});
 
 fs.writeFileSync(samplePath, JSON.stringify(data, null, 4).concat('\n'));
